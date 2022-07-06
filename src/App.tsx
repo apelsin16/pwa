9d968127690061053d8f8f845932918698a557b4
apelsin16 <apelsin@i.ua>
2022-07-06 13:28:20 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import * as React from "react";
+import { useAddToHomescreenPrompt } from "./useAddToHomescreenPrompt";
 import './App.css';
 
 function App() {
+    const [prompt, promptToInstall] = useAddToHomescreenPrompt();
+    const [isVisible, setVisibleState] = React.useState(false);
+    const hide = () => setVisibleState(false);
+
+    React.useEffect(
+        () => {
+            if (prompt) {
+                setVisibleState(true);
+            }
+        },
+        [prompt]
+    );
+
+    if (!isVisible) {
+        return (<div className="App">
+            nope
+        </div>);
+    }
+
   return (
     <div className="App">
-     nope
+        <div onClick={hide}>
+            <button onClick={hide}>Close</button>
+            Hello! Wanna add to homescreen?
+            <button onClick={promptToInstall}>Add to homescreen</button>
+        </div>
     </div>
   );
 }

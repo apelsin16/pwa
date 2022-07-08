48c756e0b3a1f88d288fdfe47fe20e8c843a0b23
apelsin16 <apelsin@i.ua>
2022-07-08 09:33:12 +0300

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import * as React from "react";
-import {useAddToHomescreenPrompt} from "./useAddToHomescreenPrompt";
+// import {useAddToHomescreenPrompt} from "./useAddToHomescreenPrompt";
 import './App.css';
 
 function App() {
-    const [prompt, promptToInstall] = useAddToHomescreenPrompt();
-    const [isVisible, setVisibleState] = React.useState(false);
-    const hide = () => setVisibleState(false);
+    // const [prompt, promptToInstall] = useAddToHomescreenPrompt();
+    // const [isVisible, setVisibleState] = React.useState(false);
+    // const hide = () => setVisibleState(false);
 
-    React.useEffect(
-        () => {
-            if (prompt) {
-                setVisibleState(true);
-            }
-        },
-        [prompt]
-    );
 
-    if (!isVisible) {
-        return (<div className="App">
-            nope
-        </div>);
-    }
+
+    // React.useEffect(
+    //     () => {
+    //         if (prompt) {
+    //             setVisibleState(true);
+    //         }
+    //     },
+    //     [prompt]
+    // );
+
+    // if (!isVisible) {
+    //     return (<div className="App">
+    //         nope
+    //     </div>);
+    // }
 
     return (
         <div className="container">
@@ -29,7 +31,7 @@ function App() {
             {/*    Hello! Wanna add to homescreen?*/}
             {/*    <button className='btn btn-primary' onClick={promptToInstall}>Add to homescreen</button>*/}
             {/*</div>*/}
-            <div className="card bg-info">
+            <div className="card bg-info p-2">
                 <img src="https://klever.blog/wp-content/uploads/2018/05/kakimi-bivaut-it-kompanii.png"
                      alt="it"
                     className='card-img-top'

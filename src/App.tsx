8e30fa7c2fac089fd8f3e10ffe279a5e61ed6099
apelsin16 <apelsin@i.ua>
2022-07-08 08:35:12 +0300

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,20 @@ function App() {
             {/*    Hello! Wanna add to homescreen?*/}
             {/*    <button className='btn btn-primary' onClick={promptToInstall}>Add to homescreen</button>*/}
             {/*</div>*/}
-            <img src="https://klever.blog/wp-content/uploads/2018/05/kakimi-bivaut-it-kompanii.png"
-                 alt="it"/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam consequuntur dolor ducimus expedita
-                facere in maxime non totam vero. Dignissimos dolor ea earum esse est facilis molestiae nihil nisi porro
-                repudiandae. Accusantium consectetur corporis dolor ducimus esse ex, excepturi fugit illo ipsum iste,
-                mollitia officiis omnis sed tempore velit?</p>
+            <div className="card bg-info">
+                <img src="https://klever.blog/wp-content/uploads/2018/05/kakimi-bivaut-it-kompanii.png"
+                     alt="it"
+                    className='card-img-top'
+                />
+                <h5 className="card-title">Card title</h5>
+                <p className='card-texr'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam consequuntur dolor ducimus expedita
+                    facere in maxime non totam vero. Dignissimos dolor ea earum esse est facilis molestiae nihil nisi porro
+                    repudiandae. Accusantium consectetur corporis dolor ducimus esse ex, excepturi fugit illo ipsum iste,
+                    mollitia officiis omnis sed tempore velit?</p>
+                <a href="#"
+                   className="btn btn-primary">Go somewhere</a>
+            </div>
+
         </div>
     );
 }
